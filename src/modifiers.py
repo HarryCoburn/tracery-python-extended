b527f04deb5f1faf7b17834faaef9fc1d0f44083
Harry Coburn <harryjcoburn@gmail.com>
2026-04-16 12:34:59 -0400

    create modifiers.py. Function definitions added.

---
+++ src/modifiers.py
@@ -0,0 +1,48 @@
+###
+# Original author of Tracery is Kate Compton
+# Conversion and upgrade by Harry Coburn
+###
+
+
+def isConsonant(c):
+    # This is done as a global var in the original.
+    pass
+
+
+def endsWithConY(s):
+    pass
+
+
+# modifiers variable is an object of functions. Those functions are below.
+
+
+def capitalizeAll(s):
+    pass
+
+
+def capitalize(s):
+    pass
+
+
+def inQuotes(s):
+    pass
+
+
+def comma(s):
+    pass
+
+
+def beeSpeak(s):
+    pass
+
+
+def a(s):
+    pass
+
+
+def s(s):
+    pass
+
+
+def ed(s):
+    pass
