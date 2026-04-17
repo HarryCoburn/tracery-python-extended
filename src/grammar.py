87bcf1680d4b47585f6781f13df93c437cb05cf6
Harry Coburn <harryjcoburn@gmail.com>
2026-04-17 10:13:08 -0400

    Update grammar.py, started symbol.py stub

--- src/grammar.py
+++ src/grammar.py
@@ -1,10 +1,29 @@
 from .modifiers import modifiers as base_modifiers
+from .symbol import Symbol
 
 
 class Grammar:
     def __init__(self) -> None:
         self.symbols = {}
-        self.modifiers = base_modifiers
+        self.modifiers = base_modifiers.copy()
+        self.symbol_names = []
 
-    def createGrammar(self, obj):
-        pass
+    def clear(self):
+        """Resets the grammar instance"""
+        self.symbols = {}
+        self.modifiers = base_modifiers.copy()
+
+    def load_from(self, obj):
+        """Loads obj into the Grammar instance"""
+        self.clear()
+        if isinstance(obj, dict) and "symbols" in obj:
+            symbol_source = obj["symbols"]
+        else:
+            symbol_source = obj
+
+        symbol_keys = obj.keys()
+
+        for idx, key in enumerate(symbol_keys):
+            self.symbol_names.append(key)
+            self.symbols[key] = Symbol(self, key)
+            self.symbols[key].load_from(symbol_source[key])
